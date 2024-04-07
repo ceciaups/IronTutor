fba3d611208c38c5a001c8194771fde65c37ca04
ceciaups <cecilia127@msn.com>
2024-04-07 11:24:04 -0400

    Update inspiration page styles

--- src/components/Inspiration.jsx
+++ src/components/Inspiration.jsx
@@ -3,7 +3,19 @@ import {useState, useEffect} from "react";
 export default function Inspiration() {
   return (
     <section id="sec-inspiration">
-
+      <h2>Inspiring Quotes</h2>
+      <div class="quotes-wrapper">
+        <div class="quotes quotes-left">
+          <img src="public/steve.jpg"/>
+          <img src="public/tom.png"/>
+          <img src="public/quote.jpg"/>
+        </div>
+        <div class="quotes quotes-right">
+          <img src="public/albert.jpg"/>
+          <img src="public/alan.jpg"/>
+          <img src="public/ali.jpg"/>
+        </div>
+      </div>
     </section>
   )
 }
